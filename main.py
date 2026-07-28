33df25ceeee09d0a0710a29a0e497368ffcf9940
wakinoza <wakinoza@gmail.com>
2026-07-28 12:56:41 +0900

    天気予報情報をメールにつ位置する実験

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# from src import weather
-# from src import format
+from src import weather
+from src import format
 from src.mailer import send_mail
 
 def main():
-    # weather_json_data = weather.fetch_forecast_json()
+    weather_json_data = weather.fetch_forecast_json()
 
-    # weather_info = weather.get_weather(weather_json_data)
+    weather_info = weather.get_weather(weather_json_data)
 
-    # print(format.format_weather(weather_info))
+    format.format_weather(weather_info)
 
     send_mail(
     "daily-notifier テスト",
-    "メール送信テストです。"
+    format.format_weather(weather_info)
 )
 
 if __name__ == "__main__":
