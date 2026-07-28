b23e9059e81174b578df1821adac470fa43ed531
wakinoza <wakinoza@gmail.com>
2026-07-28 12:48:52 +0900

    メール通知機能の追加

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from src import weather
-from src import format
+# from src import weather
+# from src import format
+from src.mailer import send_mail
 
 def main():
-    weather_json_data = weather.fetch_forecast_json()
+    # weather_json_data = weather.fetch_forecast_json()
 
-    weather_info = weather.get_weather(weather_json_data)
+    # weather_info = weather.get_weather(weather_json_data)
 
-    print(format.format_weather(weather_info))
+    # print(format.format_weather(weather_info))
+
+    send_mail(
+    "daily-notifier テスト",
+    "メール送信テストです。"
+)
 
 if __name__ == "__main__":
     main()
