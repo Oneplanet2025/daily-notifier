57961493a37ba91b2834eb69e6a3df8d928f1cd9
wakinoza <wakinoza@gmail.com>
2026-08-07 09:36:40 +0900

    共通処理を関数に切り出す

--- src/providers/weather_json.py
+++ src/providers/weather_json.py
@@ -16,26 +16,24 @@ def fetch_forecast_json():
 
     return weather_json_data
 
+
 def extract_weather_info(weather_json_data):
     """気象予報JSONから必要な情報だけ辞書として返す"""
 
     # 東部の天気予報を取得
-    forecast_area = None
-    for area in weather_json_data[0]["timeSeries"][0]["areas"]:
-        if area["area"]["name"] == JMA_FORECAST_AREA_NAME:
-            forecast_area = area
-            break
-    if forecast_area is None:
-        raise ValueError(f"{JMA_FORECAST_AREA_NAME} が見つかりません。")
+    forecast_area = find_area(
+    weather_json_data[0]["timeSeries"][0]["areas"],
+    JMA_FORECAST_AREA_NAME,
+    "天気予報",
+)
 
     # 東部の降水確率を取得
-    forecast_pop_area = None
-    for area in weather_json_data[0]["timeSeries"][1]["areas"]:
-        if area["area"]["name"] == JMA_FORECAST_AREA_NAME:
-            forecast_pop_area = area
-            break
-    if forecast_pop_area is None:
-        raise ValueError(f"{JMA_FORECAST_AREA_NAME} の降水確率が見つかりません。")
+    forecast_pop_area = find_area(
+    weather_json_data[0]["timeSeries"][1]["areas"],
+    JMA_FORECAST_AREA_NAME,
+    "降水確率",
+    )
+
     forecast_pop_times = weather_json_data[0]["timeSeries"][1]["timeDefines"]
     forecast_pops = forecast_pop_area["pops"]
     rain_forecasts = []
@@ -50,18 +48,16 @@ def extract_weather_info(weather_json_data):
 
 
     # 名古屋の気温を取得
-    temperature_area = None
-    for area in weather_json_data[0]["timeSeries"][2]["areas"]:
-        if area["area"]["name"] == JMA_TEMPERATURE_AREA_NAME:
-            temperature_area = area
-            break
-    if temperature_area is None:
-        raise ValueError(f"{JMA_TEMPERATURE_AREA_NAME} の気温情報が見つかりません。")
+    temperature_area = find_area(
+    weather_json_data[0]["timeSeries"][2]["areas"],
+    JMA_TEMPERATURE_AREA_NAME,
+    "気温情報",
+    )
 
     temperature = TemperatureInfo(
     minimum_temperature=temperature_area["temps"][-2],
     maximum_temperature=temperature_area["temps"][-1],
-)
+    )
 
     weather_info = WeatherInfo(
     report_datetime=weather_json_data[0]["reportDatetime"],
@@ -69,7 +65,16 @@ def extract_weather_info(weather_json_data):
     weather=forecast_area["weathers"][1],
     rain_forecasts=tuple(rain_forecasts),
     temperature=temperature,
-)
+    )
 
     return weather_info
+
+def find_area(areas, area_name, data_name):
+    """指定された地域のデータを取得する"""
+
+    for area in areas:
+        if area["area"]["name"] == area_name:
+            return area
+
+    raise ValueError(f"{area_name} の{data_name}が見つかりません。")
 
