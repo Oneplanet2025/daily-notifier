93dd8037a913392edace88db04bf1366ed97c59e
Hiro_M <arinkogumi@gmail.com>
2026-09-12 21:55:27 +0900

    formatter

--- src/formatters/weather_formatter.py
+++ src/formatters/weather_formatter.py
@@ -39,7 +39,7 @@ def format_weather_section(weather_info: WeatherInfo) -> str:
         pop_text += f"{format_pop_datetime(rain.time)}：{rain.probability}%\n"
     return f"""明日の天気予報
 
-明日の日付：{format_forecast_date(weather_info.forecast_date)}
+神戸の明日の日付：{format_forecast_date(weather_info.forecast_date)}
 天気：{weather_info.weather}
 
 降水確率：
